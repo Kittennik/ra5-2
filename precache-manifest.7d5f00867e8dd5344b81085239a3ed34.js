ac67296bb0f9f0f4c8e487bd5460734078bc7482
AppVeyor <ci@appveyor.com>
2020-06-26 19:46:45 +0000

    59482ce

---
+++ precache-manifest.7d5f00867e8dd5344b81085239a3ed34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ec27316717476fc560ce86f582ca65",
+    "url": "/ra5-2/index.html"
+  },
+  {
+    "revision": "cc7cc6afd98c59c1442b",
+    "url": "/ra5-2/static/css/main.70852121.chunk.css"
+  },
+  {
+    "revision": "8967ebc97281e0046bd5",
+    "url": "/ra5-2/static/js/2.d19a88a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra5-2/static/js/2.d19a88a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7cc6afd98c59c1442b",
+    "url": "/ra5-2/static/js/main.91533603.chunk.js"
+  },
+  {
+    "revision": "ebb94c964ac3253bdfa0",
+    "url": "/ra5-2/static/js/runtime-main.3c895b40.js"
+  }
+]);
